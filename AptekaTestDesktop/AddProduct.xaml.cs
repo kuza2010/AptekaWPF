6ed4b50d3d17ea2f882bd08b5777a9f566f16bcf
Danilin <kyza20106@yandex.ru>
2017-10-17 19:20:03 +0300

    Добавьте файлы проекта.

---
+++ AptekaTestDesktop/AddProduct.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AptekaTestDesktop
+{
+    /// <summary>
+    /// Логика взаимодействия для AddProduct.xaml
+    /// </summary>
+    public partial class AddProduct : Window
+    {
+        public AddProduct ()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonCancle_Click (object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ButtonOk_Click (object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NameProduct_PreviewTextInput (object sender, TextCompositionEventArgs e)
+        {
+            int val;
+            if (Int32.TryParse(e.Text, out val) && e.Text != "-")
+            {
+                e.Handled = true; // отклоняем ввод
+            }  
+        }
+
+        private void NameProduct_PreviewKeyDown (object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                e.Handled = true; // если пробел, отклоняем ввод
+            }
+        }
+    }
+}
