16c02c1aa4f78b558711e0cd8f879e115582c3fd
Danilin <kyza20106@yandex.ru>
2017-10-22 13:19:18 +0300

    Добавил функцию canvac_click. По клику мыши(любая кнопка мыши) выделяет в желтый цвет.

--- AptekaTestDesktop/MainWindow.xaml.cs
+++ AptekaTestDesktop/MainWindow.xaml.cs
@@ -20,18 +20,21 @@ namespace AptekaTestDesktop
     /// </summary>
     public partial class MainWindow : Window
     {
-      
+
         private Canvas canvas;
         int count = 0;
+        List<Label> listAmount = new List<Label>();
+        List<TextBox> listAddorDownProduct = new List<TextBox>();
+
 
         public MainWindow ()
         {
             InitializeComponent();
-          
+
         }
 
         private void ButtonAddProduct_Click (object sender, RoutedEventArgs e)
-        { 
+        {
             AddProduct windowAddProduct = new AddProduct();
             windowAddProduct.Owner = this;  //Задали отцовское окно для дочернего
             windowAddProduct.ShowDialog();  //Отобрразить как диалоговое окно
@@ -47,38 +50,40 @@ namespace AptekaTestDesktop
             //Все элементы строчки вложены в канвас
             canvas = new Canvas();
 
+            canvas.MouseDown +=(canvac_click);
             //Номер товара и имя товара, количество,добавляемое количетсво товара
             Label labelNumber = new Label();
             Label labelNameProduct = new Label();
             Label amount = new Label();
 
+            //Количество списываемого\добавлемого товара
             TextBox AddorDownAmount = new TextBox();
-
             //ТекстБокс вложен в лейбл labelNameProduct
             TextBlock textblockNameProduct = new TextBlock();
-         
 
-            
+            //добавили в листы
+            listAmount.Add(amount);
+            listAddorDownProduct.Add(AddorDownAmount);
 
             //Кнопки списать и добавить
             Button buttonAdd = new Button();
             Button buttonDown = new Button();
-            buttonAdd.Content = "Пополнить";   
+            buttonAdd.Content = "Пополнить";
             buttonDown.Content = "Списать";
 
             //Добавление всех элементов в канвас
             canvas.Children.Add(labelNumber);
             canvas.Children.Add(labelNameProduct);
-            canvas.Children.Add(amount);
+            canvas.Children.Add(listAmount[count]);
             canvas.Children.Add(buttonAdd);
-            canvas.Children.Add(AddorDownAmount);
+            canvas.Children.Add(listAddorDownProduct[count]);
             canvas.Children.Add(buttonDown);
             //установитиь для канваса строку в гриде
             Grid.SetRow(canvas, count);
 
             //установка свойст для счетчика товара(лейбл)
             Canvas.SetTop(labelNumber, 11);
-            labelNumber.Content = (count + 1).ToString() + "."; //ИЗМЕНИТЬ --------------------------------------------------------
+            labelNumber.Content = (count + 1).ToString(); //ИЗМЕНИТЬ --------------------------------------------------------
             labelNumber.Height = 54;
             labelNumber.Width = 32;
             labelNumber.HorizontalContentAlignment = HorizontalAlignment.Center;
@@ -101,10 +106,13 @@ namespace AptekaTestDesktop
             Canvas.SetLeft(amount, 233);
             amount.Height = 54;
             amount.Width = 54;
-            amount.Template = (ControlTemplate)FindResource("TextBlockTemplate");
+            amount.FontSize = 25;
+            amount.Template = (ControlTemplate) FindResource("TextBlockTemplate");
             amount.HorizontalContentAlignment = HorizontalAlignment.Center;
             amount.VerticalContentAlignment = VerticalAlignment.Center;
+            amount.Content = 0;
             amount.Background = new SolidColorBrush(Colors.White);
+            amount.Name = "IndexRow_" + labelNumber.Content.ToString();
 
             //Установка свойств для кнопки добавить
             Canvas.SetTop(buttonAdd, 23);
@@ -116,42 +124,55 @@ namespace AptekaTestDesktop
             buttonAdd.Background = new SolidColorBrush(Colors.White);
             buttonAdd.Template = (ControlTemplate) TryFindResource("ButtonAddorDownTemplate");
             buttonAdd.Click += new RoutedEventHandler(ButtonAdd_Click); //нажатие кнопки
-            //buttonAdd.Click += (sender, args) => MessageBox.Show(this, "Ура!");
+            buttonAdd.Name = "IndexRow_" + labelNumber.Content.ToString();
+
 
             //Установка свойст для товара который хотим списать/положить (лейбл)
             Canvas.SetTop(AddorDownAmount, 13);
             Canvas.SetLeft(AddorDownAmount, 405);
             AddorDownAmount.Height = 54;
             AddorDownAmount.Width = 54;
-            AddorDownAmount.Template = (ControlTemplate)FindResource("TextBoxTemplate");
+            AddorDownAmount.Template = (ControlTemplate) FindResource("TextBoxTemplate");
             AddorDownAmount.FontSize = 25;
-            AddorDownAmount.PreviewTextInput += new TextCompositionEventHandler(textBox_PreviewTextInput); //событие,для проверки ввода в textbox только цифр
+            AddorDownAmount.Text = "0";
+            AddorDownAmount.PreviewTextInput += new TextCompositionEventHandler(textBox_PreviewTextInput); //событие,для проверки ввода в textbox только цифр  
             AddorDownAmount.HorizontalContentAlignment = HorizontalAlignment.Center;
             AddorDownAmount.VerticalContentAlignment = VerticalAlignment.Center;
             AddorDownAmount.Background = new SolidColorBrush(Colors.White);
-            
+            AddorDownAmount.MaxLength = 3; //максимальное число 999
+            AddorDownAmount.Name = "IndexRow_" + labelNumber.Content.ToString();
+
             //Установка свойств для кнопки списать
             Canvas.SetTop(buttonDown, 23);
             Canvas.SetLeft(buttonDown, 459);
             buttonDown.Height = 30;
             buttonDown.Width = 100;
+            buttonDown.Click += new RoutedEventHandler(ButtonDown_Click); //нажатие кнопки
             buttonDown.HorizontalContentAlignment = HorizontalAlignment.Center;
             buttonDown.VerticalContentAlignment = VerticalAlignment.Center;
             buttonDown.Background = new SolidColorBrush(Colors.White);
+            buttonDown.Name = "IndexRow_" + labelNumber.Content.ToString();
             buttonDown.Template = (ControlTemplate) TryFindResource("ButtonAddorDownTemplate");
-
 
             AllProducts.Children.Add(canvas);
 
-            
-
             count++;
-
-
         }
 
+        private void Canvas_MouseDown (object sender, MouseButtonEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void canvac_click (object sender, MouseButtonEventArgs e)
+        {
+            ((Canvas) sender).Background = new SolidColorBrush(Colors.Yellow);
+        }
+
+        /// //////////////////////////////////////////////////////////////////////////////
         private void ButtonDelProduct_Click (object sender, RoutedEventArgs e)
         {
+            /*
             foreach (var gridChild in AllProducts.Children)
             {
                 var canv = gridChild as Canvas;
@@ -160,23 +181,86 @@ namespace AptekaTestDesktop
                    foreach(var q in canv.Children)
                     {
                         var labl = q as Label;
-                        if (q != null)
-                            labl.Content = "qwe";
+                        if (labl != null)
+                        {
+                         break;
+                        }
                     }
                 }
-            }
+            }*/
 
-            if (Grid.GetRow(canvas)==0&& Grid.GetColumn(canvas) == 0)
-            ButtonDelProduct.Content = "eewq";
+            if (Grid.GetRow(canvas) == 0 && Grid.GetColumn(canvas) == 0)
+                ButtonDelProduct.Content = "eewq";
         }
-        void textBox_PreviewTextInput (object sender, TextCompositionEventArgs e) //функция,проверяющая ввод только цифр в textbox
+        /// //////////////////////////////////////////////////////////////////////// 
+     
+
+        //функция,проверяющая ввод только цифр в textbox
+        void textBox_PreviewTextInput (object sender, TextCompositionEventArgs e) 
         {
             if (!char.IsDigit(e.Text, 0)) e.Handled = true;
+            if (((TextBox) sender).Text == "0")
+                e.Handled = true;
         }
 
+        //Кнопка добавления товара
         private void ButtonAdd_Click (object sender, RoutedEventArgs e)
         {
+            //Получение нужного нам индекса
+            string name = ((Button) sender).Name;
+            int indexRow = Int32.Parse(name.Remove(0, 9));
 
+            //Максимальное количество товара 999
+            //расчет кол-ва товара ведет функция GetAmountProduct
+            int amountProduct = GetAmountProduct(Int32.Parse(listAmount[indexRow - 1].Content.ToString()), Int32.Parse(listAddorDownProduct[indexRow - 1].Text.ToString()), "Add");
+            if (amountProduct >= 1)
+            {
+                listAmount[indexRow - 1].Content = amountProduct.ToString();
+                listAddorDownProduct[indexRow - 1].Text = "";
+            }
+        }
+
+        //Кнопка списания товара
+        private void ButtonDown_Click (object sender, RoutedEventArgs e)
+        {
+            //Получение нужного нам индекса
+            string name = ((Button) sender).Name;
+            int indexRow = Int32.Parse(name.Remove(0, 9));
+
+            //Максимальное количество товара 999
+            //расчет кол-ва товара ведет функция GetAmountProduct
+            int amountProduct = GetAmountProduct(Int32.Parse(listAmount[indexRow - 1].Content.ToString()), Int32.Parse(listAddorDownProduct[indexRow - 1].Text.ToString()), "Take");
+            if (amountProduct >= 0)
+            {
+                listAmount[indexRow - 1].Content = amountProduct.ToString();
+                listAddorDownProduct[indexRow - 1].Text = "";
+            }
+        }
+
+        //проверка на списание\добавку товара
+        private int GetAmountProduct (int amountProduct, int addOrDown, string operation)
+        {
+            //случай с добавленим товара
+            if (operation == "Add")
+            {
+                if (amountProduct + addOrDown > 999)
+                {
+                    MessageBox.Show("Будет ошибка");
+                    return -1;
+                }
+                else
+                    return amountProduct + addOrDown;
+            }
+            //Случай с отниманием товара
+            if (operation == "Take")
+                if (amountProduct - addOrDown < 0)
+                {
+                    MessageBox.Show("Будет ошибка");
+                    return -1;
+                }
+                else
+                    return amountProduct - addOrDown;
+            return 0;
         }
     }
 
